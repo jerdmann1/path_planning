b3d660e53782ac6bd7ccddca5afc4a4d81802a82
jerdmann1 <jdpaulblart@gmail.com>
2020-10-27 13:40:25 -0500

    updated command function

--- path.c
+++ path.c
@@ -16,10 +16,10 @@ bool is_unseen(char c){
     return c == '_';
 }
 
-void print_room(char room[16][10]){
+void print_room(char room[17][11]){
     printf("\x1b[2J");
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < 17; i++){
+        for(int j = 0; j < 11; j++){
             if(room[i][j] == 0){
                 printf("%5c", 'G');
             }
@@ -33,12 +33,14 @@ void print_room(char room[16][10]){
     }
 }
 
-void bfs(char room[16][10], int goal[2]){
-    Element q[1600];
+void bfs(char room[17][11], int goal[2]){
+    Element q[1700];
     int cnt = 0, orig_cnt = 0, prev_orig_cnt = 0, step = 1, u = 0;
     Element temp = {goal[0], goal[1]};
     q[orig_cnt++] = temp;
     while((orig_cnt - prev_orig_cnt) > 0){
+        print_room(room);
+        sleep(1);
         for(int i = prev_orig_cnt; i < orig_cnt; i++){
             if(q[i].x - 1 >= 0){
                 if(is_unseen(room[q[i].x - 1][q[i].y])){
@@ -48,7 +50,7 @@ void bfs(char room[16][10], int goal[2]){
                     u++;
                 }
             }
-            if(q[i].x + 1 < 16){
+            if(q[i].x + 1 < 17){
                 if(is_unseen(room[q[i].x + 1][q[i].y])){
                     room[q[i].x + 1][q[i].y] = step + '0';
                     Element temp = {q[i].x + 1, q[i].y};
@@ -64,7 +66,7 @@ void bfs(char room[16][10], int goal[2]){
                     u++;
                 }
             }
-            if(q[i].y + 1 < 10){
+            if(q[i].y + 1 < 11){
                 if(is_unseen(room[q[i].x][q[i].y + 1])){
                     room[q[i].x][q[i].y + 1] = step + '0';
                     Element temp = {q[i].x, q[i].y + 1};
@@ -81,7 +83,7 @@ void bfs(char room[16][10], int goal[2]){
     }
 }
 
-void get_commands(int commands[50], char room[16][10], int goal[2], int start[2]){
+void get_commands(int commands[50], char room[17][11], int goal[2], int start[2]){
     int curr[2] = {start[0], start[1]};
     int i = 0, orientation = 1;
     while(goal[0] != curr[0] || goal[1] != curr[1]){
@@ -98,7 +100,7 @@ void get_commands(int commands[50], char room[16][10], int goal[2], int start[2]
                 }
             }
         }
-        if(curr[0] + 1 < 16){
+        if(curr[0] + 1 < 17){
             if(room[curr[0]+1][curr[1]] != 'X'){
                 if(room[curr[0]+1][curr[1]] < min){
                     min = room[curr[0]+1][curr[1]];
@@ -118,7 +120,7 @@ void get_commands(int commands[50], char room[16][10], int goal[2], int start[2]
                 }
             }
         }
-        if(curr[1] + 1 < 10){
+        if(curr[1] + 1 < 11){
             if(room[curr[0]][curr[1]+1] != 'X'){
                 if(room[curr[0]][curr[1]+1] < min){
                     min = room[curr[0]][curr[1]+1];
@@ -163,6 +165,16 @@ void get_commands(int commands[50], char room[16][10], int goal[2], int start[2]
                 commands[i++] = -2;
                 commands[i++] = 1;
                 orientation = -1;
+            }else if(comm == -1 && orientation == 1){
+                commands[i++] = 2;
+                commands[i++] = 2;
+                commands[i++] = 1;
+                orientation = -1;
+            }else if(comm == 1 && orientation == -1){
+                commands[i++] = 2;
+                commands[i++] = 2;
+                commands[i++] = 1;
+                orientation = 1;
             }
         }else{
             commands[i++] = 1;
@@ -192,14 +204,14 @@ bool check_box(int i, int j){
 }
 
 int main(){
-    char room[16][10] = {'_'};
+    char room[17][11] = {'_'};
     int commands[50];
     int i = 0;
     for(int i = 0; i < 50; i++){
         commands[i] = 0;
     }
-    for(int i = 0; i < 16; i++){
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < 17; i++){
+        for(int j = 0; j < 11; j++){
             if(round(start[0] / 0.305) == i && round(start[1] / 0.305) == j){
                 room[i][j] = 'S';
             }else if(round(goal[0] / 0.305) == i && round(goal[1] / 0.305) == j){
