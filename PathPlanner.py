cd87f23ca8e64032bb2085da4ee8b06a708749b2
mpzinke <mathew.zinke@hotmail.com>
2020-10-30 14:06:40 -0500

    Add comments (only because I have to)

--- PathPlanner.py
+++ PathPlanner.py
@@ -8,7 +8,8 @@ __author__ = "MPZinke"
 #	on ..
 #
 #	DESCRIPTION:
-#	BUGS:
+#	BUGS:		- Does not prevent overlap.
+#				- Does not prevent blocking off section of field.
 #	FUTURE:
 #
 ###########################################################################
@@ -25,16 +26,27 @@ ADD_BORDER = True;  # add obstacles along border
 NUMBER_OF_OBSTACLES = 69;
 
 
+# Optional function to add obstacle border based on ADD_BORDER global bool.
+# Takes 2D list of field.
+# Goes through outer points and sets value to the SQUARE_SIZE factor.
+# Returns number of obstacles used to do this process.
 def create_border(field):
 	for x in range(COLUMNS): field[0][x] = field[ROWS-1][x] = SQUARE_SIZE;
-	for x in range(ROWS): field[x][0] = field[x][COLUMNS-1] = SQUARE_SIZE;
+	for x in range(1, ROWS-1): field[x][0] = field[x][COLUMNS-1] = SQUARE_SIZE;
 	return COLUMNS * 2 + (ROWS - 2) * 2;
 
 
+# Create a field for the give dimensions.
+# Returns a 2D array of Rows x Columns.
 def create_field():
 	return [[0 for x in range(COLUMNS)] for y in range(ROWS)];
 
 
+# Creates & adds random obstacles to the board.
+# Takes the field array, the maximum number of obstacles allowed to create.
+# Picks an orientation (0: horizontal, 1: vertical), finds an obstacle length that will work.
+# Picks a random position. Adds SQUARE_SIZE to parts of obstacles.
+# Returns length (number of obstacles used).
 def create_obstacles(field, remaining_obstacles):
 	orient = randint(0, 1);  # 0: horizontal, 1: vertical
 	length = randint(1, min(remaining_obstacles, [COLUMNS, ROWS][orient] - 3));  # - 3 to make it passable
@@ -44,19 +56,26 @@ def create_obstacles(field, remaining_obstacles):
 	return length;
 
 
-def print_field(field):
-	print(end=" ");
+# Prints the field, optionally with col/row numbers.
+# Takes the 2D list of the field.
+# Prints square by square (with X meaning filled, - meaning empty).
+def print_field(field, with_markers=True):
+	if(with_markers): print(end=" ");
 	for y in range(COLUMNS): print("%3d" % (y), end="");
-	print();
+	if(with_markers): print();
 
 	for x in range(ROWS):
-		print("%-2d" % (x), end="");
+		if(with_markers): print("%-2d" % (x), end="");
 		for y in range(COLUMNS):
 			print(' - ' if not field[x][y] else ' X ', end='');
 		print();
 	print();
 
 
+# Prints C_code array.
+# Takes the 2D list field.
+# Prints the declaration.  Then the instantiation.
+# Formats it to be pretty :)
 def print_C_code(field):
 	print("double obstacle[{}][2] =".format(NUMBER_OF_OBSTACLES));
 	for x in range(ROWS):
